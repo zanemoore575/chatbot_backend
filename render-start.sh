ff34e94ec1678581150b956e40b1dddb6e07297f
Zane Moore <zane.moore575@gmail.com>
2025-07-10 19:45:11 +1200

    Update render-start.sh file

--- render-start.sh
+++ render-start.sh
@@ -3,6 +3,10 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Set the RAILS_ENV to production to ensure the correct database
+# configuration is used for the migration.
+export RAILS_ENV=production
+
 echo "--- Running database migrations ---"
 bundle exec rails db:migrate
 
