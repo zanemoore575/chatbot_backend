347d759de414c824dfae5f2b81de938d2280340d
Zane Moore <zane.moore575@gmail.com>
2025-07-10 14:24:43 +1200

    Fix: Change chat route to GET for streaming

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
   # Defines the route for the chat functionality.
-  # A POST request to /chat will be handled by the 'chat' action
-  # in the 'application' controller.
-  post '/chat', to: 'application#chat'
+  # We use a GET request here because the browser's EventSource API,
+  # used for streaming, can only make GET requests.
+  get '/chat', to: 'application#chat'
 
   # You can define other routes for your application here.
-  # For example, a root route:
-  # root "articles#index"
 end
